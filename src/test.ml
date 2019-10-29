6f3ab8a098f1eea219a379add56992391b9bfb86
vagrant <vagrant@vm3110-2019fa>
2019-10-28 23:44:35 -0400

    Updated Makefile and test suite

--- src/test.ml
+++ src/test.ml
@@ -1,13 +1,31 @@
 open OUnit2
 
-let _tests = 
+let table_tests = 
+  [
+    "name" >:: (fun _ -> ());
+  ]
+
+let row_tests = 
+  [
+    "name" >:: (fun _ -> ());
+  ]
+
+let log_tests = 
+  [
+    "name" >:: (fun _ -> ());
+  ]
+
+let command_tests = 
   [
     "name" >:: (fun _ -> ());
   ]
 
 let suite =
   "test suite for keyDB"  >::: List.flatten [
-    _tests;
+    table_tests;
+    row_tests;
+    log_tests;
+    command_tests;
   ]
 
 let _ = run_test_tt_main suite
