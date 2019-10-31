a4d064cf8a099fb4d75b68c7b2d3476f676de6bb
Donal <dml333@cornell.edu>
2019-10-31 14:33:19 -0400

    Implement basic  database functions

---
+++ src/database.ml
@@ -0,0 +1,41 @@
+type t = unit
+
+exception Table_Not_Found
+
+exception Table_Exists
+
+let dir = "databases"
+
+(** [list_to_csv string list] takes a string list and returns
+ * its entries as a string to be input to a csv. The words will be
+ * in the same order as they were in the list. *)
+let list_to_csv cols =   
+  let rev = List.rev cols in 
+  let rec tail_to_csv init = function
+    | [] -> init
+    | h::t -> h ^ ", " ^ init
+  in (tail_to_csv "" (List.tl rev)) ^ (List.hd rev)
+
+let create_table name cols = 
+  let file = (dir ^ Filename.dir_sep ^ name) in
+  if Sys.file_exists file then
+    raise Table_Exists
+  else  
+    let _ = Sys.command ("touch " ^ file) in
+    let _ = Sys.command ({|echo "key, |} ^ list_to_csv cols ^ {|" >> |} ^ file)
+    in
+    print_endline ("Created table: " ^ name)
+
+let drop_table name = 
+  let table = (dir ^ Filename.dir_sep ^ name) in
+  let code = Sys.command ("rm " ^ table) in
+  if code = 0 then  
+    print_endline ("Dropped table: " ^ name)
+  else 
+    raise Table_Not_Found
+
+let read filename =
+  ()
+
+let write table =
+  ()
---
+++ src/database.mli
@@ -0,0 +1,29 @@
+(** High level management for the DBMS *)
+
+(** the type of database *)
+type t
+
+(** Raised when a table is not found. *)
+exception Table_Not_Found
+
+(** Raised when attempting to create a table that already exists. *)
+exception Table_Exists
+
+(** [dir] is the directory of the database, likely databases. *)
+val dir : string
+
+(** [create_table name cols] will build a new table in a csv file, [name].csv 
+ * the columns for this table will be the various strings within [cols]. *)
+val create_table : string -> string list -> t
+
+(** [drop_table name] will remove the table associated with [name] from
+ * the database. *)
+val drop_table : string -> t 
+
+(** [read filename] will read the csv file with name [filename] and convert
+ * it into a Table structure. *)
+val read : string -> unit 
+
+(** [write table] will look at a [table], convert it to a csv file, and 
+ * store it in the database directory. *)
+val write : string -> unit
